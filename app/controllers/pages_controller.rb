c78da68d814d11e05767a1819ab1cef09d542be3
Romain BOBOE <rboboe@gmail.com>
2017-08-07 13:11:34 -0400

    new rai app. website home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,32 @@
 class PagesController < ApplicationController
+
   def home
+    @motivation_quotes = [
+      {
+      quote: "A creative man is motivated by the desire to achieve, not by the desire to beat others.",
+      autor: "Ayn Rand"
+      },
+      {
+       quote: "If you don't design your own life plan, chances are you'll fall into someone else's plan. And guess what they have planned for you? Not much.",
+       autor: "Jim Ronh"
+      }
+  ]
+
   end
+
+  def now
+  end
+
+  def myworks
+  end
+
+  def mywritings
+  end
+
+  def myworld
+  end
+
+  def contact
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/now'
+
+  get 'pages/myworks'
+
+  get 'pages/mywritings'
+
+  get 'pages/myworld'
+
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
