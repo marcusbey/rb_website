1b5d017c8c6a8b2e612c5d7878f045467a3ca4a7
Romain BOBOE <rboboe@gmail.com>
2017-08-08 16:33:27 -0400

    push menu on the side

--- app/assets/javascripts/plugin/sidebar-menu.js
+++ app/assets/javascripts/plugin/sidebar-menu.js
@@ -1,47 +1,74 @@
+var main = function() {
+  /* Push the body and the nav over by 285px over */
+  $('#menu-sidebar-list-icon').click(function() {
+    $('.pushmenu-right').animate({
+      right: "0px"
+    }, 200);
 
-// ==============================================
-// Right Sidemenu Bar
-// ==============================================
-$(document).ready(function () {
-    $menuSidebar = $('.pushmenu-right');
-    $menusidebarNav = $('#menu-sidebar-list-icon');
-    $menuSidebarclose = $('#menu-sidebar-close-icon');
+    $('.banner').animate({
+      left: "0px"
+    }, 200);
+  });
 
-    //sidebar menu navigation icon toggle
-    $menusidebarNav.click(function () {
-        $(this).toggleClass('active');
-        $('.pushmenu-push').toggleClass('pushmenu-push-toleft pushmenu-active');
-        $menuSidebar.toggleClass('pushmenu-open');
+  /* Then push them back */
+  $('#menu-sidebar-close-icon').click(function() {
+    $('.pushmenu-right').animate({
+      right: "0px"
+    }, 200);
+
+    $('.banner').animate({
+      left: "0px"
+    }, 200);
+  });
+};
 
 
-    });
+$(document).ready(main);
 
-    //sidebar menu close icon
-    $menuSidebarclose.click(function () {
-        $menusidebarNav.removeClass('active');
-        $('.pushmenu-push').removeClass('pushmenu-push-toleft');
-        $menuSidebar.removeClass('pushmenu-open');
 
-    });
 
-    //Window Click Sidemenu Hide
-    $('html').click(function (sMenuOutsite) {
-        // Then find a target: element you clicked on.
-        var target = $(sMenuOutsite.target);
 
-        // Close your sidebar only if you clicked outside of this sidebar:
-        if ((target.closest($menusidebarNav).length === 0) && (target.closest($menuSidebarclose).length === 0) && (target.closest('.pushmenu').length === 0)) {
-            $menusidebarNav.removeClass('active');
-            $('.pushmenu-push').removeClass('pushmenu-push-toleft');
-            $menuSidebar.removeClass('pushmenu-open')
-        }
-    });
+// // ==============================================
+// // Right Sidemenu Bar
+// // ==============================================
+// $(document).ready(function () {
+//     $menuSidebar = $('.pushmenu-right');
+//     $menusidebarNav = $('#menu-sidebar-list-icon');
+//     $menuSidebarclose = $('#menu-sidebar-close-icon');
 
-    if ($(document).find('#pushmenu-right').hasClass('pushmenu')) {
-        $('body').addClass('pushmenu-push pushmenu-push-left');
-        $('body').removeClass('pushmenu-push-right');
-    }
+//     //sidebar menu navigation icon toggle
+//     $menusidebarNav.click(function () {
+//         $(this).toggleClass('active');
+//         $('.pushmenu-push').toggleClass('pushmenu-push-toleft pushmenu-active');
+//         $menuSidebar.toggleClass('pushmenu-open');
+//     });
 
-});
+//     //sidebar menu close icon
+//     $menuSidebarclose.click(function () {
+//         $menusidebarNav.removeClass('active');
+//         $('.pushmenu-push').removeClass('pushmenu-push-toleft');
+//         $menuSidebar.removeClass('pushmenu-open');
+
+//     });
+
+//     //Window Click Sidemenu Hide
+//     $('html').click(function (sMenuOutsite) {
+//         // Then find a target: element you clicked on.
+//         var target = $(sMenuOutsite.target);
+
+//         // Close your sidebar only if you clicked outside of this sidebar:
+//         if ((target.closest($menusidebarNav).length === 0) && (target.closest($menuSidebarclose).length === 0) && (target.closest('.pushmenu').length === 0)) {
+//             $menusidebarNav.removeClass('active');
+//             $('.pushmenu-push').removeClass('pushmenu-push-toleft');
+//             $menuSidebar.removeClass('pushmenu-open')
+//         }
+//     });
+
+//     if ($(document).find('#pushmenu-right').hasClass('pushmenu')) {
+//         $('body').addClass('pushmenu-push pushmenu-push-left');
+//         $('body').removeClass('pushmenu-push-right');
+//     }
+
+// });
 
 
